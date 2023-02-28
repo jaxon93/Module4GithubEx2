555e3610adad16a94e5d1739d86de52443335f36
jaxon93 <jaxon932002@gmail.com>
2023-03-01 00:53:40 +0900

    I created second activity and than added new button to open second activity on main activity

--- app/src/main/java/com/example/module4githubex2/MainActivity.kt
+++ app/src/main/java/com/example/module4githubex2/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.module4githubex2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+    private fun initView() {
+        var openSecondAc = findViewById<Button>(R.id.open_second_activity)
+        openSecondAc.setOnClickListener {
+            openSecondActivity()
+        }
+    }
+    private fun openSecondActivity() {
+        var intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
